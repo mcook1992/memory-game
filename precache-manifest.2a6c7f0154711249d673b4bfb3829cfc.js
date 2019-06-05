f42d51fa66dfd52ce56075d00bf686e248ad3999
Mercer Cook <mercercook@dyn-160-39-205-155.dyn.columbia.edu>
2019-06-04 22:38:25 -0400

    Updates

---
+++ precache-manifest.2a6c7f0154711249d673b4bfb3829cfc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee3b6148fee91b6119b560db3a39b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b905059d44c82737d998",
+    "url": "/static/css/main.04447cf2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b905059d44c82737d998",
+    "url": "/static/js/main.b5112c26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4412feb0d563fd07be2003df044ee2f",
+    "url": "/static/media/Bad-Joke-Eel.c4412feb.jpg"
+  },
+  {
+    "revision": "d34168193ada68b092f1a37c3ee3d090",
+    "url": "/static/media/Business-Cat.d3416819.jpg"
+  },
+  {
+    "revision": "90756013c4af210339622af6e061956a",
+    "url": "/static/media/Chemistry-Cat.90756013.jpg"
+  },
+  {
+    "revision": "5dcf23401f3ac7de1d3d5ae407ed4908",
+    "url": "/static/media/Condescending-Wonka.5dcf2340.jpg"
+  },
+  {
+    "revision": "5f7b5c0cd7a11fcb37f3c0b206d8e3fd",
+    "url": "/static/media/Drunk-Baby.5f7b5c0c.jpg"
+  },
+  {
+    "revision": "3de027c382f0b27b2f5c782eb8b1a6d2",
+    "url": "/static/media/First-World-Problems.jpg.3de027c3.webp"
+  },
+  {
+    "revision": "86c35e085d5b854102536d686686ab07",
+    "url": "/static/media/Frye.jpg..86c35e08.webp"
+  },
+  {
+    "revision": "6c2282033411b92dd0b08aeafef8ccc0",
+    "url": "/static/media/Grumpy-Cat.6c228203.jpg"
+  },
+  {
+    "revision": "babf952648b88fafaa5bc4d3b16c9593",
+    "url": "/static/media/Hipster-Dog.jpg.babf9526.webp"
+  },
+  {
+    "revision": "43f42d3650429ac8bcb330cac6e07fce",
+    "url": "/static/media/Its-the-Children.43f42d36.jpg"
+  },
+  {
+    "revision": "942def0003f634065e0cfaa46d8d2ae7",
+    "url": "/static/media/Most-Interesting-Man.942def00.jpg"
+  },
+  {
+    "revision": "5d20431a6110dc1b5f84b366f95b1c18",
+    "url": "/static/media/grumpy-cat-meme-fun.5d20431a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
