1ef61f4892e77ce42257b0648cc7eebf5b132dec
Mercer Cook <mercercook@Mercers-MacBook-Pro.local>
2020-06-02 11:22:24 -0400

    added some readability changes

--- src/components/board.js
+++ src/components/board.js
@@ -3,38 +3,15 @@ import Card from "./card";
 import Header from "./header";
 import arrayOfCards from "../arrayFile";
 
-var repeatClick = false;
-
-var gameLost = false;
-
-function shuffle(array) {
-  console.log("shuffling the array");
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
 class Board extends Component {
   state = {
     score: 0,
-    wins: 0
+    wins: 0,
+    gameLost: false,
   };
 
-  handleSquareClicked = event => {
+  //function we'll use to handle each time a user clicks a square
+  handleSquareClicked = (event) => {
     //preventing auto page reload
     event.preventDefault();
 
@@ -45,18 +22,19 @@ class Board extends Component {
 
     //checking if the player has lost
 
-    arrayOfCards.forEach(element => {
+    arrayOfCards.forEach((element) => {
       if (element.id == currentNodeID) {
+        //if the element has been clicked before
         if (element.hasBeenClicked == true) {
           alert("You lost");
-          this.setState({ score: 0 }, function() {
+          this.setState({ score: 0 }, function () {
             //resetting the array after a loss
-            arrayOfCards.forEach(element => {
+            arrayOfCards.forEach((element) => {
               element.hasBeenClicked = false;
             });
-            shuffle(arrayOfCards);
+            this.shuffle(arrayOfCards);
           });
-          gameLost = true;
+          this.state.gameLost = true;
         } else {
           //if that image hasn't already been clicked, now mark it as has been clicked
           element.hasBeenClicked = true;
@@ -66,35 +44,50 @@ class Board extends Component {
 
     //if they haven't lost the game
 
-    if (gameLost == false) {
-      console.log("entering the if gamelost false function");
-      this.setState({ score: this.state.score + 1 }, function() {
-        //if they've clicked all twelve without losing, tell them they won!
-
+    if (this.state.gameLost == false) {
+      //add a point to their score
+      this.setState({ score: this.state.score + 1 }, function () {
+        //if they've clicked all twelve without losing, tell them they won and reset the cards
         if (this.state.score > 11) {
           alert("You won");
-          this.setState({ score: 0, wins: this.state.wins + 1 }, function() {
-            arrayOfCards.forEach(element => {
+          this.setState({ score: 0, wins: this.state.wins + 1 }, function () {
+            arrayOfCards.forEach((element) => {
               element.hasBeenClicked = false;
             });
-            // shuffle(arrayOfCards);
           });
-        } else {
-          console.log("entering the else clause of the game lost function");
-          // shuffle(arrayOfCards);
         }
       });
     } else {
       //if they have lost the game, skip all the steps above and just reset the deck
-      gameLost = false;
-      arrayOfCards.forEach(element => {
+      this.state.gameLost = false;
+      arrayOfCards.forEach((element) => {
         element.hasBeenClicked = false;
       });
     }
 
-    shuffle(arrayOfCards);
+    this.shuffle(arrayOfCards);
+  };
 
-    //go through and see if the player has lost
+  //The function we'll use to shuffle the cards so that they're not in the same place every time
+
+  shuffle = (array) => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there are still enough elements left to shuffle, we will move them around the screen
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   };
 
   render() {
@@ -102,7 +95,7 @@ class Board extends Component {
       <div className="mainBoard">
         <Header score={this.state.score} wins={this.state.wins} />
         <div className="board">
-          {arrayOfCards.map(card => (
+          {arrayOfCards.map((card) => (
             <Card
               number={card.cardNumber}
               hasBeenClicked={card.hasBeenClicked}
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,11 @@ import React from "react";
 function Header({ score, wins }) {
   return (
     <div>
-      <p>Welcome to the game</p>
+      <h1>Welcome to Meme Memory Match! </h1>
+      <p>
+        Click on a tile to increase your score. But don't click the same tile
+        twice!
+      </p>
       <p>score:{score}</p>
       <p>wins: {wins}</p>
     </div>
